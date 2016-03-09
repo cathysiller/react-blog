6741cb798685070b9a23c15a8b34279c45685879
Cathy Siller <catherine.siller@springbox.com>
2016-03-08 22:49:36 -0600

    added in header/body/footer

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import Body from './body'
-import Header from './header'
-import Footer from './footer'
-
 
 class App extends React.Component {
 	render() {
 		return (
-			<span>
-			     <Header />
-			     <Body />
-			     <Footer />
-			</span>
+			<Body />
 		)
 	}
 }
---
+++ components/body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Body extends React.Component {
+	render() {
+		return (
+			<span>
+			<h1>First blog post</h1>
+			<p className="date">enter date here</p>
+			<p className="post">text for first blog post here</p>
+			</span>
+		)
+	}
+}
+
+export default Body;
